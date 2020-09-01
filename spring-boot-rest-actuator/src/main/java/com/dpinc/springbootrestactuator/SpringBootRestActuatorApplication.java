7976487594252fd3273d0520b5c729fdae973399
Dhruvesh Patel <dmpatel.1412@gmail.com>
2020-09-01 21:36:44 +0100

    Added spring boot actuator project

---
+++ spring-boot-rest-actuator/src/main/java/com/dpinc/springbootrestactuator/SpringBootRestActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.dpinc.springbootrestactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestActuatorApplication.class, args);
+	}
+
+}
